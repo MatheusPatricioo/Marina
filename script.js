56be89ab8cd9ad7f4615aab4ee858b0172ad2577
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-16 12:56:03 -0300

    up

--- script.js
+++ script.js
@@ -25,7 +25,18 @@ function sim() {
     // Cria um coração gigante com a mensagem de texto e respostas anteriores
     var coracaoContainer = document.getElementById('coracaoContainer');
     var coracao = document.createElement('div');
-    coracao.innerHTML = '&hearts; <span class="mensagem">Você tem um encontro marcado<br>' + respostasAnteriores + '</span>';
+    
+    // Obtém a data atual para incluir na mensagem
+    var data = new Date();
+    var dataFormatada = data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+    
+    // Obtém as respostas anteriores
+    var respostas = respostasAnteriores.split("<br>");
+    
+    // Cria a mensagem final com as respostas
+    var mensagemFinal = "Agora você tem um encontro marcado para o dia " + dataFormatada + ", vamos comer " + respostas[0].split(":")[1].trim() + " e vamos passar no " + respostas[1].split(":")[1].trim();
+    
+    coracao.innerHTML = '&hearts; <span class="mensagem">' + mensagemFinal + '</span>';
     coracao.className = 'coracao-gigante';
     coracaoContainer.appendChild(coracao);
     // Redireciona após alguns segundos
@@ -33,6 +44,7 @@ function sim() {
         window.location.href = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
     }, 5000); // 5 segundos
 }
+
 
 function desvia(btn) {
     // Move o botão para uma nova posição aleatória na telaa.
@@ -52,6 +64,3 @@ document.getElementById('btnSim').addEventListener('click', function() {
     // Mostra a próxima pergunta
     perguntaPrincipalRespondida('Sim');
 });
-
-
-/* coloquei no servidor, agr falta ajustar o hover */
