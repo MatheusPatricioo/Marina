8b675c65c7d9a1ec7b703417542987a7000849fd
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-16 13:44:20 -0300

    up

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var respostasAnteriores = ''; // Variável global para armazenar as respostas anteriores
 
-function pergunta1Respondida(resposta) {
-    console.log("Resposta pergunta 1: " + resposta);
-    // Armazena a resposta anterior
-    respostasAnteriores += "Pergunta 1: " + resposta + "<br>";
-    // Oculta a pergunta atual e mostra a próxima
-    document.getElementById('pergunta1').style.display = 'none';
-    document.getElementById('pergunta2').style.display = 'block';
-}
-
 function pergunta2Respondida(resposta) {
     console.log("Resposta pergunta 2: " + resposta);
     // Armazena a resposta anterior
     respostasAnteriores += "Pergunta 2: " + resposta + "<br>";
     // Oculta a pergunta atual e mostra a próxima
     document.getElementById('pergunta2').style.display = 'none';
+    document.getElementById('pergunta1').style.display = 'block';
+}
+
+function pergunta1Respondida(resposta) {
+    console.log("Resposta pergunta 1: " + resposta);
+    // Armazena a resposta anterior
+    respostasAnteriores += "Pergunta 1: " + resposta + "<br>";
+    // Oculta a pergunta atual e mostra a próxima
+    document.getElementById('pergunta1').style.display = 'none';
+    document.getElementById('pergunta3').style.display = 'block';
+}
+
+function pergunta3Respondida(resposta) {
+    console.log("Resposta pergunta 3: " + resposta);
+    // Armazena a resposta anterior
+    respostasAnteriores += "Pergunta 3: " + resposta + "<br>";
+    // Oculta a pergunta atual e mostra a próxima
+    document.getElementById('pergunta3').style.display = 'none';
+    document.getElementById('pergunta4').style.display = 'block';
+}
+
+function pergunta4Respondida(resposta) {
+    console.log("Resposta pergunta 4: " + resposta);
+    // Armazena a resposta anterior
+    respostasAnteriores += "Pergunta 4: " + resposta + "<br>";
+    // Oculta a pergunta atual e mostra a próxima
+    document.getElementById('pergunta4').style.display = 'none';
     document.getElementById('perguntaPrincipal').style.display = 'block';
 }
 
@@ -33,8 +51,9 @@ function sim() {
     // Obtém as respostas anteriores
     var respostas = respostasAnteriores.split("<br>");
     
-    // Cria a mensagem final com as respostas
-    var mensagemFinal = "Agora você tem um encontro marcado para o dia " + dataFormatada + ", vamos comer " + respostas[0].split(":")[1].trim() + " e vamos passar no " + respostas[1].split(":")[1].trim();
+    // Cria a mensagem final com as respostas, excluindo a quarta resposta
+    var mensagemFinal = "Agora você tem um encontro marcado para o dia " + dataFormatada + ", vamos comer " + respostas[0].split(":")[1].trim() + ", depois vamos da um role de  " + respostas[1].split(":")[1].trim() + " e ";
+    mensagemFinal += "eu tenho uma surpresa especial para você <3 ";
     
     coracao.innerHTML = '&hearts; <span class="mensagem">' + mensagemFinal + '</span>';
     coracao.className = 'coracao-gigante';
@@ -45,22 +64,11 @@ function sim() {
     }, 5000); // 5 segundos
 }
 
-
 function desvia(btn) {
-    // Move o botão para uma nova posição aleatória na telaa.
+    // Move o botão para uma nova posição aleatória na tela
     var newPositionX = Math.random() * (window.innerWidth - 100);
     var newPositionY = Math.random() * (window.innerHeight - 100);
     btn.style.position = 'absolute';
     btn.style.top = newPositionY + 'px';
     btn.style.left = newPositionX + 'px';
 }
-
-document.getElementById('btnNao').addEventListener('click', function() {
-    // Mostra a próxima pergunta
-    perguntaPrincipalRespondida('Não');
-});
-
-document.getElementById('btnSim').addEventListener('click', function() {
-    // Mostra a próxima pergunta
-    perguntaPrincipalRespondida('Sim');
-});
